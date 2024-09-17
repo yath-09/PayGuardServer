7e984e716bb5c986609c8e16549bd31df592facc
yatharth <yatharth0903@gmail.com>
2024-09-17 22:50:39 +0530

    db and server setup

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors'
+import userRoutes from "./routes/user.routes.js"
+dotenv.config(); // Load environment variables before using them
 const app = express();
 const port = process.env.PORT || 8000;
 
+
 app.use(express.json());
+//cors prevention
+app.use(cors({ origin: process.env.CORS_ORIGIN,credentials:true }));
 
-
-app.get('/', (req, res) => {
-  res.send('Hello PayGuard!');
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port: ${port}`));
+  })
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
+ 
+app.get('/api', (req, res) => {
+  const data = 'Hello PayGuard!';
+  res.send(data);
 });
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
-});
+//test api 
+app.use('/api/user',userRoutes) //imp use instead if method
